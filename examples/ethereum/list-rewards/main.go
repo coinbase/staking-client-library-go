bf8c75ae835399e37ee674c22112c0dab0498932
Marcin Lenczewski <marcin.lenczewski@coinbase.com>
2024-05-16 16:09:03 -0400

    README updates for PES rewards

--- examples/ethereum/list-rewards/main.go
+++ examples/ethereum/list-rewards/main.go
@@ -11,56 +11,82 @@ import (
 	"log"
 	"time"
 
+	"google.golang.org/api/iterator"
+	"google.golang.org/protobuf/encoding/protojson"
+
 	"github.com/coinbase/staking-client-library-go/auth"
 	"github.com/coinbase/staking-client-library-go/client"
 	"github.com/coinbase/staking-client-library-go/client/options"
 	"github.com/coinbase/staking-client-library-go/client/rewards"
 	filter "github.com/coinbase/staking-client-library-go/client/rewards/rewardsfilter"
 	api "github.com/coinbase/staking-client-library-go/gen/go/coinbase/staking/rewards/v1"
-	"google.golang.org/api/iterator"
-	"google.golang.org/protobuf/encoding/protojson"
 )
-
-/*
- * Run the code with 'go run examples/ethereum/list-rewards/main.go' to view the rewards for the first validator on the Ethereum network.
- * Or, to view rewards for any arbitrary validator, simply replace the public key below.
- */
-
-const (
+ 
+ const (
 	apiKeyName    = "your-api-key-name"
-	apiPrivateKey = "your-api-private-key"
-
-	// https://beaconcha.in/validator/1
-	address = "0xa1d1ad0714035353258038e964ae9675dc0252ee22cea896825c01458e1807bfad2f9969338798548d9858a571f7425c"
-)
-
-func main() {
-	ctx := context.Background()
-
-	// Loads the API key.
-	apiKey, err := auth.NewAPIKey(auth.WithAPIKeyName(apiKeyName, apiPrivateKey))
-	if err != nil {
+    apiPrivateKey = "your-api-private-key"
+	
+	 partialETHAddress = "0x60c7e246344ae3856cf9abe3a2e258d495fc39e0"
+	 // https://beaconcha.in/validator/1
+	 validatorAddress = "0xa1d1ad0714035353258038e964ae9675dc0252ee22cea896825c01458e1807bfad2f9969338798548d9858a571f7425c"
+	)
+	
+ 
+ func main() {
+	 ctx := context.Background()
+ 
+	 // Loads the API key.
+	 apiKey, err := auth.NewAPIKey(auth.WithAPIKeyName(apiKeyName, apiPrivateKey))
+	 if err != nil {
 		log.Fatalf("error loading API key: %s", err.Error())
-	}
+	 }
+ 
+	 // Creates the Coinbase Staking API client
+	 stakingClient, err := client.New(ctx, options.WithAPIKey(apiKey))
+	 if err != nil {
+		 log.Fatalf("error instantiating staking client: %s", err.Error())
+	 }
+ 
+	  // Lists the rewards for the given partial eth address for May 1st 2024, aggregated by day.
+	 partialETHRewardsIter := stakingClient.Rewards.ListRewards(ctx, &api.ListRewardsRequest{
+		 Parent:   rewards.Ethereum,
+		 PageSize: 200,
+		 Filter: filter.WithAddress().Eq(partialETHAddress).
+			 And(filter.WithPeriodEndTime().Gte(time.Date(2024, 5, 1, 0, 0, 0, 0, time.Local))).
+			 And(filter.WithPeriodEndTime().Lt(time.Date(2024, 5, 2, 0, 0, 0, 0, time.Local))).String(),
+	 })
 
-	// Creates the Coinbase Staking API client
-	stakingClient, err := client.New(ctx, options.WithAPIKey(apiKey))
-	if err != nil {
-		log.Fatalf("error instantiating staking client: %s", err.Error())
-	}
+	  // Lists the rewards for the given validator address between February 25, 2024 and February 27th 2024, aggregated by day.
+	  validatorRewardsIter := stakingClient.Rewards.ListRewards(ctx, &api.ListRewardsRequest{
+        Parent:   rewards.Ethereum,
+        PageSize: 200,
+        Filter: filter.WithAddress().Eq(validatorAddress).
+		And(filter.WithPeriodEndTime().Gte(time.Date(2024, 2, 25, 0, 0, 0, 0, time.Local))).
+		And(filter.WithPeriodEndTime().Lt(time.Date(2024, 2, 27, 0, 0, 0, 0, time.Local))).String(),
+    })
+ 
+	 // Iterates through the rewards and pretty print them.
+	 for {
+		 reward, err := partialETHRewardsIter.Next()
+		 if errors.Is(err, iterator.Done) {
+			 break
+		 }
+ 
+		 if err != nil {
+			 log.Fatalf("error listing rewards: %s", err.Error())
+		 }
+ 
+		 marshaled, err := protojson.MarshalOptions{Indent: "  ", Multiline: true}.Marshal(reward)
+		 if err != nil {
+			 log.Fatalf("error marshaling reward: %s", err.Error())
+		 }
+ 
+		 fmt.Println(string(marshaled))
+	 }
 
-	// Lists the rewards for the given address for the previous last 20 days, aggregated by day.
-	rewardsIter := stakingClient.Rewards.ListRewards(ctx, &api.ListRewardsRequest{
-		Parent:   rewards.Ethereum,
-		PageSize: 200,
-		Filter: filter.WithAddress().Eq(address).
-			And(filter.WithPeriodEndTime().Gte(time.Now().AddDate(0, 0, -20))).
-			And(filter.WithPeriodEndTime().Lt(time.Now())).String(),
-	})
-
-	// Iterates through the rewards and pretty print them.
-	for {
-		reward, err := rewardsIter.Next()
+	 // Iterates through the rewards and pretty print them.
+	 for {
+		reward, err := validatorRewardsIter.Next()
 		if errors.Is(err, iterator.Done) {
 			break
 		}
@@ -69,12 +95,12 @@ func main() {
 			log.Fatalf("error listing rewards: %s", err.Error())
 		}
 
-		marshaler := protojson.MarshalOptions{Indent: "\t"}
-		marshaled, err := marshaler.Marshal(reward)
+		marshaled, err := protojson.MarshalOptions{Indent: "  ", Multiline: true}.Marshal(reward)
 		if err != nil {
 			log.Fatalf("error marshaling reward: %s", err.Error())
 		}
 
 		fmt.Println(string(marshaled))
 	}
-}
+
+ }
