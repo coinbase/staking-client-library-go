a27be9a70b391d827e10451db0526ab6342b92f2
Marcin Lenczewski <marcin.lenczewski@coinbase.com>
2024-05-16 17:04:51 -0400

    More linting formatting fixes

--- examples/ethereum/list-rewards/partial-eth/main.go
+++ examples/ethereum/list-rewards/partial-eth/main.go
@@ -21,55 +21,54 @@ import (
 	filter "github.com/coinbase/staking-client-library-go/client/rewards/rewardsfilter"
 	api "github.com/coinbase/staking-client-library-go/gen/go/coinbase/staking/rewards/v1"
 )
- 
- const (
+
+const (
 	apiKeyName    = "your-api-key-name"
 	apiPrivateKey = "your-api-private-key"
-	
-	 partialETHAddress = "0x60c7e246344ae3856cf9abe3a2e258d495fc39e0"
-	)
-	
- 
- func main() {
-	 ctx := context.Background()
- 
-	 // Loads the API key.
-	 apiKey, err := auth.NewAPIKey(auth.WithAPIKeyName(apiKeyName, apiPrivateKey))
-	 if err != nil {
-		log.Fatalf("error loading API key: %s", err.Error())
-	 }
- 
-	 // Creates the Coinbase Staking API client
-	 stakingClient, err := client.New(ctx, options.WithAPIKey(apiKey))
-	 if err != nil {
-		 log.Fatalf("error instantiating staking client: %s", err.Error())
-	 }
- 
-	  // Lists the rewards for the given partial eth address between May 1st, 2024 and May 3rd, 2024 aggregated by day.
-	 partialETHRewardsIter := stakingClient.Rewards.ListRewards(ctx, &api.ListRewardsRequest{
-		 Parent:   rewards.Ethereum,
-		 PageSize: 200,
-		 Filter: filter.WithAddress().Eq(partialETHAddress).
-			 And(filter.WithPeriodEndTime().Gte(time.Date(2024, 5, 1, 0, 0, 0, 0, time.Local))).
-			 And(filter.WithPeriodEndTime().Lt(time.Date(2024, 5, 3, 0, 0, 0, 0, time.Local))).String(),
-	 })
 
-	 // Iterate through the partial eth rewards and pretty print them.
-	 for {
-		 reward, err := partialETHRewardsIter.Next()
-		 if errors.Is(err, iterator.Done) {
-			 break
-		 }
- 
-		 if err != nil {
-			 log.Fatalf("error listing rewards: %s", err.Error())
-		 }
- 
-		 marshaled, err := protojson.MarshalOptions{Indent: "  ", Multiline: true}.Marshal(reward)
-		 if err != nil {
-			 log.Fatalf("error marshaling reward: %s", err.Error())
-		 }
- 
-		 fmt.Println(string(marshaled))
-	 }
- }
+	partialETHAddress = "0x60c7e246344ae3856cf9abe3a2e258d495fc39e0"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Loads the API key.
+	apiKey, err := auth.NewAPIKey(auth.WithAPIKeyName(apiKeyName, apiPrivateKey))
+	if err != nil {
+		log.Fatalf("error loading API key: %s", err.Error())
+	}
+
+	// Creates the Coinbase Staking API client
+	stakingClient, err := client.New(ctx, options.WithAPIKey(apiKey))
+	if err != nil {
+		log.Fatalf("error instantiating staking client: %s", err.Error())
+	}
+
+	// Lists the rewards for the given partial eth address between May 1st, 2024 and May 3rd, 2024 aggregated by day.
+	partialETHRewardsIter := stakingClient.Rewards.ListRewards(ctx, &api.ListRewardsRequest{
+		Parent:   rewards.Ethereum,
+		PageSize: 200,
+		Filter: filter.WithAddress().Eq(partialETHAddress).
+			And(filter.WithPeriodEndTime().Gte(time.Date(2024, 5, 1, 0, 0, 0, 0, time.Local))).
+			And(filter.WithPeriodEndTime().Lt(time.Date(2024, 5, 3, 0, 0, 0, 0, time.Local))).String(),
+	})
+
+	// Iterate through the partial eth rewards and pretty print them.
+	for {
+		reward, err := partialETHRewardsIter.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("error listing rewards: %s", err.Error())
+		}
+
+		marshaled, err := protojson.MarshalOptions{Indent: "  ", Multiline: true}.Marshal(reward)
+		if err != nil {
+			log.Fatalf("error marshaling reward: %s", err.Error())
+		}
+
+		fmt.Println(string(marshaled))
+	}
+}
